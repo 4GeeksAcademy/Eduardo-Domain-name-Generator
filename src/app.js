64ae448d459e43dbd450327c3aa1b27f3cd037d3
Eduardo7052311 <124112815+Eduardo7052311@users.noreply.github.com>
2024-08-14 22:01:32 +0000

    generador nombre de dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+pronoun.forEach(articulo => {
+  adj.forEach(adj => {
+    noun.forEach(suj => {
+      console.log(articulo + adj + suj + ".com");
+    });
+  });
+});
